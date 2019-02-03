b82b5ce46109a4c7f72f9d443f6247145c3a9ecc
Wilbur78 <cwhardy23@yahoo.com>
2019-02-03 07:33:23 -0800

    Second commit

--- app/src/main/java/java/example/android/moodtracker/MainActivity.java
+++ app/src/main/java/java/example/android/moodtracker/MainActivity.java
@@ -1,14 +1,15 @@
 package java.example.android.moodtracker;
 
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
@@ -16,49 +17,55 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
-
-import java.example.android.moodtracker.RecyclerView.Recycler;
 
 import static android.view.View.*;
 
-public class MainActivity extends AppCompatActivity implements
-        GestureDetector.OnGestureListener {
+public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener {
 
     private static final String TAG = "Gestures";
     private GestureDetectorCompat mDetector;
     private ImageView mSmileyFace;
-    public String comment;
-    private int [] moodList = {
-            R.drawable.mood_ecstatic,
-            R.drawable.mood_happy,
-            R.drawable.mood_neutral,
+    private RelativeLayout mRelativeLayout;
+    private ImageButton mHistory;
+    public static String comment;
+    public static final int [] [] moodList = {
+            {R.drawable.mood_sad,
             R.drawable.mood_kind_of_sad,
-            R.drawable.mood_sad};
-    private int moodIndex;
-    private static final int NUM_LIST_ITEMS = 100;
-    private Recycler mAdapter;
-    private RecyclerView mNumbersList;
+            R.drawable.mood_neutral,
+            R.drawable.mood_happy,
+            R.drawable.mood_ecstatic},
+            {R.color.faded_red,
+            R.color.warm_grey,
+            R.color.cornflower_blue_65,
+            R.color.light_sage,
+            R.color.banana_yellow}};
+    public static int moodIndex = 3;
+    SharedPreferences mSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mDetector = new GestureDetectorCompat(this,this);
 
-        mSmileyFace = (ImageView) findViewById(R.id.imageView1);
+        mSmileyFace = findViewById(R.id.imageView1);
+        mRelativeLayout = findViewById(R.id.main_menu_relative_layout);
+        mHistory = findViewById(R.id.moodHistory);
+        mDetector = new GestureDetectorCompat(this, this);
 
-//        RecyclerView mNumbersList = findViewById(R.id.rv_numbers);
-//
-//        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-//        mNumbersList.setLayoutManager(layoutManager);
-//
-//        mNumbersList.setHasFixedSize(true);
-//
-//        mAdapter = new Recycler(NUM_LIST_ITEMS);
-//
-//        mNumbersList.setAdapter(mAdapter);
+
+        mSettings = getApplicationContext().getSharedPreferences("Preference", MODE_PRIVATE);
+
+        mSettings.registerOnSharedPreferenceChangeListener(
+                new SharedPreferences.OnSharedPreferenceChangeListener() {
+                    @Override
+                    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+                        mSettings.getInt("Preference", moodIndex);
+                    }
+                }
+        );
 
         ImageButton btnShow = findViewById(R.id.btnShow);
         btnShow.setOnClickListener(new OnClickListener() {
@@ -97,77 +104,79 @@ public class MainActivity extends AppCompatActivity implements
             }
         });
 
-        ImageButton imageButton = (ImageButton) findViewById(R.id.moodHistory);
-        final Context context = this;
-        imageButton.setOnClickListener(new OnClickListener() {
-
+        mHistory.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent SharedPreferences = new Intent(context, MainActivity.class);
-                startActivity(SharedPreferences);
+                Intent intent = new Intent(MainActivity.this, moodHistory.class);
+                startActivity(intent);
             }
         });
     }
 
     @Override
-    public boolean onDown(MotionEvent event) {
-        Log.d(TAG,"onDown: ?" + event.toString());
-        return true;
+    public boolean onDown(MotionEvent e) {
+        return false;
     }
 
     @Override
-    public void onLongPress(MotionEvent event) {
-        Log.d(TAG, "onLongPress: ?" + event.toString());
+    public void onShowPress(MotionEvent e) {
+
     }
 
     @Override
-    public boolean onScroll(MotionEvent event1, MotionEvent event2, float distanceX,
-                            float distanceY) {
-        Log.d(TAG, "onScroll: ?" + event1.toString() + event2.toString());
-        return true;
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
     }
 
     @Override
-    public void onShowPress(MotionEvent event) {
-        Log.d(TAG, "onShowPress: ?" + event.toString());
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
     }
 
     @Override
-    public boolean onSingleTapUp(MotionEvent event) {
-        Log.d(TAG, "onSingleTapUp: ?" + event.toString());
-        return true;
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    private void prefEdit() {
+        SharedPreferences.Editor editor = mSettings.edit();
+        editor.putInt("Mood Index Value", moodIndex);
+        editor.apply();
     }
 
     @Override
-    public boolean onFling(MotionEvent e1, MotionEvent e2,
-                           float velocityX, float velocityY) {
-
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
         Log.d(TAG, "onFling: started");
 
-        if(moodIndex < 4 && e1.getY() - e2.getY() > 50) {
+        if (moodIndex < 4 && e1.getY() - e2.getY() > 50) {
             moodIndex++;
+            mSmileyFace.setImageResource(moodList[1][moodIndex]);
+            mRelativeLayout.setBackgroundColor(getResources().getColor(moodList[0][moodIndex]));
 
-            mSmileyFace.setImageResource(moodIndex);
-
+            prefEdit();
 
             Toast.makeText(MainActivity.this, "You swiped up :D",
-                    Toast.LENGTH_LONG). show();
-
-            return true;
+                    Toast.LENGTH_LONG).show(); return true;
 
         } else if (moodIndex > 0 && e2.getY() - e1.getY() > 50) {
 
             Log.d(TAG, "onFling: called");
             moodIndex--;
+            mSmileyFace.setImageResource(moodList[1][moodIndex]);
+            mRelativeLayout.setBackgroundColor(getResources().getColor(moodList[0][moodIndex]));
+
+            prefEdit();
 
             Toast.makeText(MainActivity.this, "You swiped down :/",
-                    Toast.LENGTH_LONG). show();
+                    Toast.LENGTH_LONG).show(); return true;
 
-            return true;
         } else {
-
             return true;
         }
+    }
+
+    public void setComment() {
+        comment = "";
     }
 }
 
